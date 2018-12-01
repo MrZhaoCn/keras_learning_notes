142331707ebef77e829933adf48b4a9e7e4230a4
zhaocun <zhaocun@youzan.com>
2018-12-01 21:14:31 +0800

    电影评价model

---
+++ movie_evaluate/movie_evaluate_model.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+imdb_dir = '../data/aclImdb'
+train_dir = os.path.join(imdb_dir, 'train')
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+maxlen = 100
+training_samples = 200
+validation_samples = 10000
+max_words = 10000
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+word_index = tokenizer.word_index
+
+
+data = pad_sequences(sequences, maxlen=maxlen)
+labels = np.asarray(labels)
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples: training_samples + validation_samples]
+y_val = labels[training_samples: training_samples + validation_samples]
+
+#词嵌入
+embeddings_index = {}
+f = open('../data/glove.6B.100d.txt')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print(len(embeddings_index))
